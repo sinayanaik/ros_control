b0d0813b07e58297b369499884e7ba06778e3508
Sinaya Naik <sinayanaik@gmail.com>
2025-06-27 23:37:39 +0530

    both arm and gripper controller is working

--- robot_controller/launch/controllers.launch.py
+++ robot_controller/launch/controllers.launch.py
@@ -1,88 +1,49 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
 from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
+from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
-
-    is_sim_arg = DeclareLaunchArgument(
-        "is_sim",
-        default_value="True"
+    # Load controllers configuration
+    controller_config = os.path.join(
+        get_package_share_directory("robot_controller"),
+        "config",
+        "robot_controllers.yaml"
     )
 
-    is_sim = LaunchConfiguration("is_sim")
-
-    robot_description = ParameterValue(
-        Command(
-            [
-                "xacro ",
-                os.path.join(
-                    get_package_share_directory("robot_description"),
-                    "urdf",
-                    "robot.urdf.xacro",
-                ),
-            ]
-        ),
-        value_type=str,
-    )
-
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        condition=UnlessCondition(is_sim),
-        parameters=[{"robot_description": robot_description}],
-    )
-
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[
-            {"robot_description": robot_description,
-             "use_sim_time": is_sim},
-            os.path.join(
-                get_package_share_directory("robot_controller"),
-                "config",
-                "robot_controllers.yaml",
-            ),
-        ],
-        condition=UnlessCondition(is_sim),
-    )
-
+    # Load controllers
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=[
-            "joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
+        arguments=["joint_state_broadcaster"],
+        output="screen",
+        parameters=[controller_config],
     )
 
-    arm_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["arm_controller", "--controller-manager", "/controller_manager"],
+    # Delay start of robot_controller after `joint_state_broadcaster_spawner`
+    delayed_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[
+                Node(
+                    package="controller_manager",
+                    executable="spawner",
+                    arguments=["arm_controller", "gripper_controller"],
+                    output="screen",
+                    parameters=[controller_config],
+                )
+            ],
+        )
     )
 
-    gripper_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["gripper_controller", "--controller-manager", "/controller_manager"],
-    )
-
-    return LaunchDescription(
-        [
-            is_sim_arg,
-            robot_state_publisher_node,
-            controller_manager,
-            joint_state_broadcaster_spawner,
-            arm_controller_spawner,
-            gripper_controller_spawner,
-        ]
-    )
+    return LaunchDescription([
+        joint_state_broadcaster_spawner,
+        delayed_controller_spawner,
+    ])
